3967957cb1c3896bab30e3f64d6678b41b272ca4
Sneha Kumari (Goyal) <snehabaranwal5613@gmail.com>
2025-01-16 02:03:21 +0530

    first commit

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
   res.send('Recipe API is running...');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
